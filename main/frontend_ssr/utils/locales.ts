4aeda3c1a1c47543a92f29014796512fdef5ec92
Martin Hanák <martinhanak97@gmail.com>
2024-05-19 20:22:34 +0200

    SSG pre-rendering

---
+++ main/frontend_ssr/utils/locales.ts
@@ -0,0 +1,3 @@
+export const locales = ["cs", "en"];
+
+export const defaultLocale = "en";
--- main/frontend_ssr/vite.config.ts
+++ main/frontend_ssr/vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react({}), ssr({})],
+  plugins: [react({}), ssr({ prerender: true })],
   server: {
     port: 8080,
     strictPort: true,
